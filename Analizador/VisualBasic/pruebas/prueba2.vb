71b69f1cc65a2f68716b743322565d7273dae32f
Marco Wan Martinez <marcowan213@hotmail.com>
2021-01-08 17:44:00 -0600

    Analizador Sintactico

--- Analizador/VisualBasic/pruebas/prueba2.vb
+++ Analizador/VisualBasic/pruebas/prueba2.vb
@@ -1,15 +1,7 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())        
-        Dim a As Integer = 10
-        Dim b As Integer = 5
-        Dim firstCheck, secondCheck As Boolean
-        firstCheck = Not (a > b)
-        secondCheck = Not (b > a)
-        Console.WriteLine($"{firstCheck} | {secondCheck}")
-        Console.WriteLine(vbCrLf + "Cualquier cosa para salir")
-        a = a+b 
+    Sub Main(args As String())
         Console.WriteLine(a)
         Console.ReadKey(True)
     End Sub
